fadabe00c93c059a5e6a05f0c88e7f1baea737da
mizanur211053 <mizanur211053@gmail.com>
2026-06-05 16:28:30 +0600

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -31,7 +31,7 @@ BATCH_FILES = {}
 async def start(client, message):
     sticker = None
     try:
-        stick_id = "CAACAgUAAxkBAAEQJmJpViid_0yscWKPfh3RMCY8pIkmXwACMAcAAqzbsFexyKU6FPQAAjgE"
+        stick_id = "CAACAgUAAxkBAAERVktqIqSOZKGqjsj-QeSPd8Pq-AchGgACQxwAAqa-GFX7Fh5GyTkI-TsE"
         try:
             sticker = await message.reply_sticker(sticker=stick_id)
         except Exception as e:
