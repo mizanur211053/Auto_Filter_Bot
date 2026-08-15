66358db424ae3bf4f8f5955f86dfca4eda96ac57
mizanur211053 <mizanur211053@gmail.com>
2026-08-15 17:25:45 +0600

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -107,7 +107,7 @@ async def start(client, message):
             await client.send_message(LOG_CHANNEL, script.LOG_TEXT_P.format(message.from_user.id, message.from_user.mention))
         if len(message.command) != 2:
             buttons = [[
-                        InlineKeyboardButton('🔰 ᴀᴅᴅ ᴍᴇ ᴛᴏ ʏᴏᴜʀ ɢʀᴏᴜᴘ 🔰', url=f'http://t.me/{temp.U_NAME}?startgroup=true')
+                        InlineKeyboardButton('°•Buy Premium Subscription•°', url=f'http://t.me/{temp.U_NAME}?start=premium')
                     ],[
                         InlineKeyboardButton(' ʜᴇʟᴘ 📢', callback_data='help'),
                         InlineKeyboardButton(' ᴀʙᴏᴜᴛ 📖', callback_data='about')
