4b73272b231f2bcad432a5521c1a8e48a9d010cb
mizanur211053 <mizanur211053@gmail.com>
2026-08-22 20:20:18 +0600

    Update pmfilter.py

--- plugins/pmfilter.py
+++ plugins/pmfilter.py
@@ -109,7 +109,7 @@ async def refercall(bot, query):
             'invite link', url=f'https://telegram.me/share/url?url=https://t.me/{bot.me.username}?start=reff_{query.from_user.id}&text=Hello%21%20Experience%20a%20bot%20that%20offers%20a%20vast%20library%20of%20unlimited%20movies%20and%20series.%20%F0%9F%98%83'),
         InlineKeyboardButton(
             f'⏳ {referdb.get_refer_points(query.from_user.id)}', callback_data='ref_point'),
-        InlineKeyboardButton('Back', callback_data='premium_info')
+        InlineKeyboardButton('Back', callback_data='start')
     ]]
     reply_markup = InlineKeyboardMarkup(btn)
     try:
