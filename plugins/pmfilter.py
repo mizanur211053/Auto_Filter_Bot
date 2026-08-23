fcb07a32b4c2d6864354bba825f955b312460296
mizanur211053 <mizanur211053@gmail.com>
2026-08-23 09:20:03 +0600

    Update pmfilter.py

--- plugins/pmfilter.py
+++ plugins/pmfilter.py
@@ -28,7 +28,7 @@ logger.setLevel(logging.ERROR)
 tracemalloc.start()
 
 
-TIMEZONE = "Asia/Kolkata"
+TIMEZONE = "Asia/Dhaka"
 BUTTON = {}
 BUTTONS = {}
 FRESH = {}
@@ -109,7 +109,7 @@ async def refercall(bot, query):
             'invite link', url=f'https://telegram.me/share/url?url=https://t.me/{bot.me.username}?start=reff_{query.from_user.id}&text=Hello%21%20Experience%20a%20bot%20that%20offers%20a%20vast%20library%20of%20unlimited%20movies%20and%20series.%20%F0%9F%98%83'),
         InlineKeyboardButton(
             f'⏳ {referdb.get_refer_points(query.from_user.id)}', callback_data='ref_point'),
-        InlineKeyboardButton('Back', callback_data='start')
+        InlineKeyboardButton(' ⇋ Back ⇋ ', callback_data='start')
     ]]
     reply_markup = InlineKeyboardMarkup(btn)
     try:
@@ -1612,7 +1612,7 @@ async def cb_handler(client: Client, query: CallbackQuery):
         try:
             btn = [[
                 InlineKeyboardButton('ꜱᴛᴀʀ', callback_data='star_info'),
-                InlineKeyboardButton('BKS/NGD', callback_data='upi_info')
+                InlineKeyboardButton('ʙᴋs/ɴɢᴅ', callback_data='upi_info')
             ],[
                 InlineKeyboardButton('⇋ ʙᴀᴄᴋ ᴛᴏ ᴘʀᴇᴍɪᴜᴍ ⇋', callback_data='premium_info')
             ]]
